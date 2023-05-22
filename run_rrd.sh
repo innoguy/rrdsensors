aceff225e4ff9c795a13279c7c57c92d8f751d98
Guy Coen <gcoen@cirrusled.com>
2023-05-22 19:56:07 +0800

    Corrected formula

--- run_rrd.sh
+++ run_rrd.sh
@@ -76,7 +76,7 @@ do
   if [[ $CONTROLLER == 'NUC' ]]
   then
 	  SSD_TEMP="$(bc -l <<< $(sudo smartctl -d sntrealtek /dev/$DISK -a | grep 'Temperature:' | awk '{print $2}'))"
-    if [[ $((SSD_TEMP > 128)) ]]; then SSD_TEMP=$((128-SSD_TEMP)); fi
+    if [ $SSD_TEMP > 128 ]; then SSD_TEMP=$((128-SSD_TEMP)); fi
   elif [[ $CONTROLLER == 'T1' ]]
   then
     if [[ $DISK == 'nvme0n1' ]]
@@ -85,7 +85,7 @@ do
 	  elif [[ $DISK == 'sda' ]]
 	  then 
 	    SSD_TEMP="$(bc -l <<< $(smartctl -d ata /dev/sda -a | grep 'Temperature' | awk '{print $10}'))"
-      if [ $SSD_TEMP -gt 128 ]; then SSD_TEMP=$((128-SSD_TEMP)); fi
+      if [ $SSD_TEMP > 128 ]; then SSD_TEMP=$((128-SSD_TEMP)); fi
 	  else
 	    SSD_TEMP=0
 	  fi
